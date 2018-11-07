2f10e4e3875453e82b21006ec6fe1bf74fad9d06
WildPastry <mykdsn@gmail.com>
2018-11-08 07:38:29 +1300

    Updated files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*jslint browser:true */
 
-// CREATE ARRAY
+// CREATE MOVIE ARRAY
 var allMovies = [{
         name: 'whaleRider',
         age: 'primary',
@@ -71,8 +71,8 @@ document.getElementById('filterPrimary').addEventListener('click', filterPrimary
 document.getElementById('filterSecondary').addEventListener('click', filterSecondary);
 
 //CLEAR FILTER BUTTON (DISPLAY ALL)
-function clearMovies(){
-    $(function(){
+function clearMovies() {
+    $(function () {
         $(".movie").show();
     });
     console.log(".movie")
@@ -80,23 +80,72 @@ function clearMovies(){
 
 // AGE BUTTON (FILTER BY AGE)
 //FILTER PRIMARY AGE
-function filterPrimary(){
-    $(function(){
+function filterPrimary() {
+    $(function () {
         $(".secondary").hide();
     });
-    $(function(){
+    $(function () {
         $(".primary").show();
     });
     console.log(".primary")
 }
 
 //FILTER PRIMARY AGE
-function filterSecondary(){
-    $(function(){
+function filterSecondary() {
+    $(function () {
         $(".primary").hide();
     });
-    $(function(){
+    $(function () {
         $(".secondary").show();
     });
     console.log(".secondary")
 }
+
+// WATCHLIST BUTTONS
+// CREATE EMPTY WATCHLIST
+// var watchList = [];
+// var movies = ["whaleRider"]
+// console.log(watchList);
+
+// jQuery(function ($) {
+//     $(".addWhaleBtn").click(function () {
+//         if ((watchList.includes("whaleRider")) == 1) {
+//             console.log("You have already added this movie to the Watchlist");
+//             alert("You have already added this movie to the Watchlist");
+//         }
+//         if (((watchList.includes("whaleRider")) == 0) && (movies.includes("whaleRider"))) {
+//             watchList.push("whaleRider");
+//             console.log(watchList.includes("whaleRider"));
+//             console.log(watchList);
+//             document.getElementById("watchList").innerHTML = "";
+//             for (var i = 0; i < watchList.length; i++) {
+//                 document.getElementById("watchList").innerHTML += "<li >" + watchList[i] + "</li>";
+//             }
+//         }
+//         return watchList;
+//     });
+//     console.log(document.getElementById('watchList'));
+// });
+
+
+// function addWhale() {
+
+//     document.getElementsByClassName("addWhaleBtn").addEventListener('click', addWhale);
+//     document.getElementsByClassName("watchList").innerHTML = "";
+
+//     for (var i = 0; i < 3; i++) {
+
+//         watchList = document.getElementsByClassName('watchList');
+//         createListOuter = document.createElement('div');
+//         createListInner = document.createElement('li');
+
+//         createListOuter.className = 'dropdown-menu';
+//         createListInner.className = 'dropdown-item';
+//         createListOuter.appendChild(createListInner);
+
+//         createListInner.innerHTML += '<li">' + allMovies[i].name + '</li>';
+//         watchList.appendChild(createListOuter);
+//     }
+//     console.log(document.getElementsByClassName('watchList'));
+//     console.log("Item added to Watchlist")
+// }
